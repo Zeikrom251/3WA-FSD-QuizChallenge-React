9940de909b710cf72a38e3742da9d339bcd710e7
Ryan Chikhi <ryanechikhi2004@gmail.com>
2025-02-06 13:50:11 +0100

    🐛 bug(fix): autoNext fix, if user click "next" there is no autoNext

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -15,54 +15,63 @@ const Quiz = () => {
   let Option2 = useRef(null)
   let Option3 = useRef(null)
   let Option4 = useRef(null)
-
   let option_array = [Option1, Option2, Option3, Option4]
 
+  const timeoutRef = useRef(null)
+  const autoNextRef = useRef(null)
+
   useEffect(() => {
-    if (result || lock) return
+    if (result) return
 
     const countDown = setInterval(() => {
       setTimer((prev) => (prev > 0 ? prev - 1 : 0))
     }, 1000)
 
-    const timeout = setTimeout(() => {
+    autoNextRef.current = setTimeout(() => {
       if (!lock) {
-        setLock(true)
-        option_array[question.ans - 1].current.classList.add("correct")
-        setMessage("Temps écoulé ! La bonne réponse a été sélectionnée.")
-
-        setTimeout(() => {
-          setMessage("")
-          next()
-        }, 5000)
+        handleTimeOut()
       }
     }, 30000)
 
     return () => {
       clearInterval(countDown)
-      clearTimeout(timeout)
+      clearTimeout(autoNextRef.current)
+      clearTimeout(timeoutRef.current)
     }
-  }, [index, result, lock])
+  }, [index, result])
+
+  const handleTimeOut = () => {
+    setLock(true)
+    option_array[question.ans - 1].current.classList.add("correct")
+    setMessage("⏳ Temps écoulé ! La bonne réponse a été sélectionnée.")
+
+    timeoutRef.current = setTimeout(() => {
+      setMessage("")
+      next()
+    }, 5000)
+  }
 
   const checkAns = (e, ans) => {
     if (!lock) {
       if (question.ans === ans) {
         e.target.classList.add("correct")
-        setLock(true)
         setScore((prev) => prev + 1)
       } else {
         e.target.classList.add("wrong")
-
         option_array[question.ans - 1].current.classList.add("correct")
       }
       setLock(true)
-      setTimeout(() => {
+
+      timeoutRef.current = setTimeout(() => {
+        setMessage("")
         next()
       }, 5000)
     }
   }
 
   const next = () => {
+    clearTimeout(timeoutRef.current)
+
     if (index === data.length - 1) {
       setResult(true)
       return
@@ -71,7 +80,7 @@ const Quiz = () => {
     setIndex((prev) => prev + 1)
     setQuestion(data[index + 1])
     setLock(false)
-    setTimer(30) // Réinitialisation du timer
+    setTimer(30)
 
     option_array.forEach((option) => {
       option.current.classList.remove("wrong", "correct")
@@ -79,6 +88,8 @@ const Quiz = () => {
   }
 
   const reset = () => {
+    clearTimeout(timeoutRef.current)
+    clearTimeout(autoNextRef.current)
     setIndex(0)
     setQuestion(data[0])
     setScore(0)
@@ -101,32 +112,16 @@ const Quiz = () => {
           <p className="timer">⏳ Temps restant : {timer}</p>
           {message && <p className="message">{message}</p>}
           <ul>
-            <li
-              ref={Option1}
-              onClick={(e) => {
-                checkAns(e, 1)
-              }}>
+            <li ref={Option1} onClick={(e) => checkAns(e, 1)}>
               {question.option1}
             </li>
-            <li
-              ref={Option2}
-              onClick={(e) => {
-                checkAns(e, 2)
-              }}>
+            <li ref={Option2} onClick={(e) => checkAns(e, 2)}>
               {question.option2}
             </li>
-            <li
-              ref={Option3}
-              onClick={(e) => {
-                checkAns(e, 3)
-              }}>
+            <li ref={Option3} onClick={(e) => checkAns(e, 3)}>
               {question.option3}
             </li>
-            <li
-              ref={Option4}
-              onClick={(e) => {
-                checkAns(e, 4)
-              }}>
+            <li ref={Option4} onClick={(e) => checkAns(e, 4)}>
               {question.option4}
             </li>
           </ul>
